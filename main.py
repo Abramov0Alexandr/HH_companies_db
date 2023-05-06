0b63a657b90f55ab32c48bd462cef1322290c068
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-05-06 13:19:45 +0300

    Написание чернового кода

--- main.py
+++ main.py
@@ -1,17 +1,85 @@
 import pprint
 import requests
+import csv
 
 
-def parce_company(key_words: list | str):
-    url = 'https://api.hh.ru/employers?only_with_vacancies=true'
-    params = {'text': ' & '.join(key_words),
-              'per_page': 10,
-              'area': '113'}
-    response = requests.get(url, params=params).json()['items']
-    return response
+class CompanyParser:
+
+    __employer_url = 'https://api.hh.ru/employers?only_with_vacancies=true'
+
+    def get_employers(self, key_word: str) -> list[dict]:
+        params = {'text': key_word.lower(),
+                  'per_page': 10,
+                  'area': '113'}
+        response = requests.get(self.__employer_url, params=params).json()['items']
+        return response
+
+    def get_employers_id(self, data):
+        id_list = []
+        for i in data:
+            id_list.append(i.get('id'))
+
+        return id_list
+
+    def save_as_csv(self, filename: str, data: list) -> None:
+        filename = f"{filename.title().strip()}.csv"
+
+        with open(filename, mode='w', newline='', encoding='utf-16') as csv_file:
+            fieldnames = ['company_id', 'company_title', 'vacancy_count', 'url']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for item in data:
+                writer.writerow({'company_id': item.get('id'),
+                                 'company_title': item.get('name'),
+                                 'vacancy_count': item.get('open_vacancies'),
+                                 'url': item.get('alternate_url')})
 
 
-row_data = parce_company(['IT', 'разработка'])
-print(row_data)
-pprint.pprint(row_data)
+class VacancyParser:
+
+    __vacancy_url = 'https://api.hh.ru/vacancies'
+
+    def get_employers_vacancies(self, indexes: list):
+
+        params = {'employer_id': indexes,
+                  'per_page': 100}
+        response = requests.get(self.__vacancy_url, params=params).json()['items']
+        return response
+
+    def save_as_csv(self, filename: str, data: list) -> None:
+        filename = f"{filename.title().strip()}.csv"
+
+        with open(filename, mode='w', newline='', encoding='utf-16') as csv_file:
+            fieldnames = ['company_id', 'vacancy_title',
+                          'salary_from', 'salary_to', 'url']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for item in data:
+
+                if item['salary']:
+                    salary_from = 0 if not item['salary']['from'] else item['salary']['from']
+                    salary_to = 0 if not item['salary']['to'] else item['salary']['to']
+
+                    writer.writerow(
+                        {'company_id': item['employer'].get('id'),
+                         'vacancy_title': item.get('name'),
+                         'salary_from': f"{salary_from}",
+                         'salary_to': f"{salary_to}",
+                         'url': item.get('alternate_url')})
+
+
+cp = CompanyParser()
+vp = VacancyParser()
+
+
+employers = cp.get_employers('it')
+indexes = cp.get_employers_id(employers)
+vac = vp.get_employers_vacancies(indexes)
+
+# vp.save_as_csv('test_vac', vac)
+
+print()
+
 
--- testing_place.py
+++ testing_place.py
@@ -1,8 +1,9 @@
+import json
 import pprint
 import requests
 
 
-def create_database(data_base_name: str, params: dict):
+def create_employers_database(data_base_name: str, params: dict):
     pass
 
 
@@ -10,13 +11,52 @@ def save_data_to_db(data: list[dict], db_name: str, params: dict):
     pass
 
 
-def parce_company(key_word: str):
+def parce_company(key_words: list | str):
     url = 'https://api.hh.ru/employers?only_with_vacancies=true'
-    params = {'text': key_word,
+    params = {'text': ' & '.join(key_words),
               'per_page': 10,
               'area': '113'}
     response = requests.get(url, params=params).json()['items']
     return response
 
 
+def __write_to_json_file(self, data):
+    """
+    Метод для записи переданных в качестве аргумента данных в формат JSON.
+    Метод служит для облегчения интерфейса класса
+    :param data: Данные для записи
+    """
+    with open(self.__filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+# @property
+# def __data_from_json_file(self):
+# """
+# Метод для получения данных из записанного JSON файла.
+# Метод служит для облегчения интерфейса класса
+# """
+# with open(self.__filename, encoding='utf-8') as file:
+#     vacancies = json.load(file)
+#     return vacancies
+
+
 pprint.pprint(parce_company('IT'))
+
+
+#: Тестовая заготовка черновик!!
+#: С данной функции можно будет брать данные для заполнения sql
+def employers_sql_info(data: list):
+
+    result = []
+
+    for i in data:
+        res_dict = {
+            'company_id': i.get('id'),
+            'company_title': i.get('name'),
+            'vacancy_count': i.get('open_vacancies'),
+            'url': i.get('alternate_url')}
+
+        result.append(res_dict)
+
+    return result
